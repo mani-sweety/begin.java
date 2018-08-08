6822aba2e7f8e57a454f381eaeb6de2fe7822e1c
mani-sweety <41941890+mani-sweety@users.noreply.github.com>
2018-08-08 06:06:05 +0000

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Even
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	         int n;
+        Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        if(n%2==0){
+          System.out.println("Even");
+         }
+        else{
+          System.out.println("Invalid");
+        }
+
+	}
+}
