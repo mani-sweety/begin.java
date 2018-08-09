96cd6b8b637cf9a9037ff6831ce2d3762f1dab3b
mani-sweety <41941890+mani-sweety@users.noreply.github.com>
2018-08-09 06:32:46 +0000

    Update Odd.java

--- Odd.java
+++ Odd.java
@@ -2,12 +2,12 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
+
 class Odd
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+	
 		int n,m;
 		 Scanner s = new Scanner(System.in);
         n = s.nextInt();
