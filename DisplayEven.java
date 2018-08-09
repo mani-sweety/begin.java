f401033f2ef7ff49dddf4904ff72e9474a0f8f9a
mani-sweety <41941890+mani-sweety@users.noreply.github.com>
2018-08-09 06:49:58 +0000

    Update DisplayEven.java

--- DisplayEven.java
+++ DisplayEven.java
@@ -2,12 +2,11 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
 class DisplayEven
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+	
 		int n,i,m;
          
         Scanner s = new Scanner(System.in);
