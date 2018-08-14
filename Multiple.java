c90b2958ccd40458405a2a9b5a4282dd6ea721fc
mani-sweety <41941890+mani-sweety@users.noreply.github.com>
2018-08-14 06:59:19 +0000

    Update Multiple.java

--- Multiple.java
+++ Multiple.java
@@ -2,27 +2,25 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
+
 class Multiple
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+	
 		int i;
-      int n=5;
-      int res;
+   
+     
 
       Scanner input = new Scanner(System.in);
 
          
          i = input.nextInt();
 
-        for(int j=1; j<=n;j++){
-        	
-        res=i*j;
+        for(int j=1; j<=5;j++)
 
-         System.out.print(res+" ");
-	}
+         System.out.print(" "+i*j);
+	
 		
 	}
 }
