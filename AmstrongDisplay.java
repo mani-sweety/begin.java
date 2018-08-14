da320a35bd72c92b8dfda1ef635ffc5facfef47c
mani-sweety <41941890+mani-sweety@users.noreply.github.com>
2018-08-14 06:26:20 +0000

    Update AmstrongDisplay.java

--- AmstrongDisplay.java
+++ AmstrongDisplay.java
@@ -2,12 +2,11 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
 class AmstrongDisplay
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+
 		int low, high;
 		Scanner s=new Scanner(System.in);
 		low=s.nextInt();
@@ -26,7 +25,7 @@ class AmstrongDisplay
         int result = 0;
         int originalNumber = num;
 
-        // number of digits calculation
+       
         while (originalNumber != 0) {
             originalNumber /= 10;
             ++digits;
@@ -34,7 +33,7 @@ class AmstrongDisplay
 
         originalNumber = num;
 
-        // result contains sum of nth power of its digits
+   
         while (originalNumber != 0) {
             int remainder = originalNumber % 10;
             result += Math.pow(remainder, digits);
