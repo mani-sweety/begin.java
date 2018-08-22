d92e42a320b73e9e39c7f1db4a846a2a1f3cf51a
mani-sweety <41941890+mani-sweety@users.noreply.github.com>
2018-08-22 11:04:44 +0530

    Create Set48.java

---
+++ Set48.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Set48
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		//swap(a,b);
+		a=a^b;
+		b=a^b;
+		a=a^b;
+System.out.print(a+" "+b);
+	}
+}
