c68468c107e7dbbaf0cc4404b7d5c036ff9cdbf3
mani-sweety <41941890+mani-sweety@users.noreply.github.com>
2018-08-23 12:31:48 +0530

    Create Set65.java

---
+++ Set65.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Set65
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=a*b;
+		if(c%2==0)
+		System.out.println("even");
+		else
+System.out.println("odd");
+	}
+}
