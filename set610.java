045d8c0a0239193b3baf5a7cd4c4fe428f5ce92c
mani-sweety <41941890+mani-sweety@users.noreply.github.com>
2018-08-29 12:15:32 +0530

    Create set610.java

---
+++ set610.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Set610
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int sum=0;
+		for(int i=0;i<=n;i++)
+		{
+			sum=sum+i;
+		}
+System.out.println(sum);
+	}
+}
