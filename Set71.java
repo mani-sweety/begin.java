65dc4e5e48654226c2e1d63a43e5a25de8995caa
mani-sweety <41941890+mani-sweety@users.noreply.github.com>
2018-08-29 12:22:34 +0530

    Create Set71.java

---
+++ Set71.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Set71
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String name=sc.next();
+		int k=sc.nextInt();
+		int i;
+		for(i=0;i<k;i++)
+		{
+			System.out.print(name.charAt(i));
+			
+		}
+
+	}
+}
